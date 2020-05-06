d4097039cfdce035e20f4f06532f20bb8c5dd840
ericdunipace <edunipace@g.harvard.edu>
2020-05-06 00:40:53 -0400

    maxit bug fix

--- R/W1L1.R
+++ R/W1L1.R
@@ -1,4 +1,5 @@
 W1L1 <- function(X, Y, theta = NULL, penalty = c("none", "lasso","scad","mcp"), 
+                 model.size = NULL,
                  lambda = numeric(0), 
                  lambda.min.ratio = 1e-4, 
                  nlambda = 10, 
@@ -12,12 +13,21 @@ W1L1 <- function(X, Y, theta = NULL, penalty = c("none", "lasso","scad","mcp"),
   
   n <- nrow(X)
   d <- ncol(X)
+  
+  if(is.null(Y) | missing(Y)) {
+    if(!(is.null(theta) | missing(theta))) {
+      if(nrow(theta) != ncol(X)) theta <- t(theta)
+      Y <- X %*% theta
+    }
+  }
+  
   s <- ncol(Y)
   cols <- lapply(1:s, function(ss) Matrix::sparseMatrix(i = n*(ss-1) + rep(1:n,d), 
                                                         j = rep(1:d,each = n), 
                                                         x = c(X),
                                                         dims = c(n*s, d)))
   Xmat <- do.call(cbind, cols)
+  rm(cols)
   
   if(length(lambda) == 0) {
     if(penalty != "lasso") {
@@ -29,6 +39,14 @@ W1L1 <- function(X, Y, theta = NULL, penalty = c("none", "lasso","scad","mcp"),
   }
   if(length(lambda) == 1) if(lambda == 0) penalty <- "none"
   
+  
+  if(is.null(model.size) | length(model.size) == 0) {
+    model.size <- ncol(Xmat)
+  } else {
+    model.size <- model.size * s
+  }
+  
+  
   if(penalty != "none") {
     rqpenargs <-  list(
       x = as.matrix(Xmat),
@@ -37,13 +55,14 @@ W1L1 <- function(X, Y, theta = NULL, penalty = c("none", "lasso","scad","mcp"),
       tau = 0.5,
       intercept = FALSE,
       a = gamma,
+      model.size = model.size,
       penalty = switch(penalty, "lasso" = "LASSO", 
                        "mcp" = "MCP",
                        "scad" = "SCAD"),
       lambda = lambda,
       ...)
     rqpenargs <- rqpenargs[!duplicated(names(rqpenargs))]
-    rqpenargs <- rqpenargs[names(rqpenargs) %in% names(c(formals(l1.group.fit), formals(quantreg::rq.fit.pfn), formals(quantreg::rq.fit.br), formals(quantreg::rq.fit.fnb)))]
+    
     if(is.null(rqpenargs$method)) {
       rqpenargs$method <- if(nrow(Xmat) > 1000 & ncol(Xmat) > 100) { #sfn gives error
         #   "sfn"
@@ -57,10 +76,18 @@ W1L1 <- function(X, Y, theta = NULL, penalty = c("none", "lasso","scad","mcp"),
       }
     }
     
+    
     if(length(lambda) ==1 ) {
+      rqpenargs <- rqpenargs[names(rqpenargs) %in% names(c(formals(l1.group.fit), 
+                                                             formals(quantreg::rq.fit.pfn), 
+                                                             formals(quantreg::rq.fit.br), 
+                                                             formals(quantreg::rq.fit.fnb)))]
       if(is.null(rqpenargs$alg)) rqpenargs$alg <- "QICD"
+      argn <- lapply(names(rqpenargs), as.name) 
+      names(argn) <- names(rqpenargs)
+      
       f.call <- as.call(c(list(call("::", as.name("rqPen"), 
-                                    as.name("l1.group.fit"))), rqpenargs))
+                                    as.name("l1.group.fit"))), argn))
       # res <- do.call(l1.group.fit, rqpenargs)
       # res <- l1.group.fit(x = rqpenargs$x, y = rqpenargs$y, 
       #                            groups = rqpenargs$groups, tau = 0.5, lambda = rqpenargs$lambda,
@@ -68,8 +95,16 @@ W1L1 <- function(X, Y, theta = NULL, penalty = c("none", "lasso","scad","mcp"),
       #                            penalty = rqpenargs$penalty, 
       #                            alg = rqpenargs$alg, penGroups = NULL, ...)
     } else {
+      rqpenargs <- rqpenargs[names(rqpenargs) %in% c(names(c(formals(l1.group.fit), 
+                                                             formals(quantreg::rq.fit.pfn), 
+                                                             formals(quantreg::rq.fit.br), 
+                                                             formals(quantreg::rq.fit.fnb))), 
+                                                     "model.size")]
       # if(is.null(rqpenargs$alg)) rqpenargs$alg <- "QICD_warm"
-      f.call <- as.call(c(list(as.name("rqGroupLambda")), rqpenargs))
+      argn <- lapply(names(rqpenargs), as.name) 
+      names(argn) <- names(rqpenargs) 
+      
+      f.call <- as.call(c(list(as.name("rqGroupLambda")), argn))
       # res1 <- rqGroupLambda(x = rqpenargs$x, y = rqpenargs$y,
       #                      groups = rqpenargs$groups, tau = 0.5, lambda = rqpenargs$lambda,
       #                      intercept = FALSE,
@@ -79,7 +114,7 @@ W1L1 <- function(X, Y, theta = NULL, penalty = c("none", "lasso","scad","mcp"),
       #                      ...)
       
     }
-    res <- eval(f.call, rqpenargs)
+    res <- eval(f.call, envir = rqpenargs)
     beta <- sapply(res, function(r) r$coefficients)
   } else {
     lambda <- 0
@@ -95,7 +130,6 @@ W1L1 <- function(X, Y, theta = NULL, penalty = c("none", "lasso","scad","mcp"),
     #   tau = 0.5,
     #   ...
     # )
-    rqargs <- rqargs[names(rqargs) %in% names(c(formals(quantreg::rq.fit.pfn), formals(quantreg::rq.fit.br), formals(quantreg::rq.fit.fnb)))]
     rqargs <- rqargs[!duplicated(names(rqargs))]
     if(is.null(rqargs$method)) {
       rqargs$method <- if(nrow(Xmat) > 1000 & ncol(Xmat) > 100) { #sfn gives error
@@ -109,10 +143,17 @@ W1L1 <- function(X, Y, theta = NULL, penalty = c("none", "lasso","scad","mcp"),
         "br"
       }
     }
+    arg.names <- switch(rqargs$method,
+                        "pfn" = names(c(formals(quantreg::rq.fit.pfn))), 
+                        "br" = names(formals(quantreg::rq.fit.br)), 
+                        "fn" = names(formals(quantreg::rq.fit.fnb)))
+    rqargs <- rqargs[names(rqargs) %in% arg.names ]
     
-    call <- as.call(c(list(call("::", as.name("quantreg"), 
-                                as.name("rq.fit"))), rqargs))
-    res <- eval(call, rqargs)
+    argn <- lapply(names(rqargs), as.name)
+    names(argn) <- names(rqargs)
+    f.call <- as.call(c(list(call("::", as.name("quantreg"), 
+                                as.name("rq.fit"))), argn))
+    res <- eval(f.call, envir = rqargs)
     # res <- do.call(quantreg::rq, rqargs)
     
     beta <- as.matrix(res$coefficients)
@@ -122,6 +163,7 @@ W1L1 <- function(X, Y, theta = NULL, penalty = c("none", "lasso","scad","mcp"),
   output$penalty <- penalty
   output$lambda <- lambda
   output$nvars <- d
+  output$maxit <- NULL
   output$call <- formals(W1L1)
   output$call[names(this.call)] <- this.call
   output$nonzero_beta <- colSums(output$beta != 0)
