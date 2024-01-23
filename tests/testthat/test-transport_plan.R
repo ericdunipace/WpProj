81469524e31dfbae666b878cc25d25b128391c20
ericdunipace <edunipace@mail.harvard.edu>
2024-01-22 22:28:39 -0800

    fix cran issues

--- tests/testthat/test-transport_plan.R
+++ tests/testthat/test-transport_plan.R
@@ -152,6 +152,7 @@ testthat::test_that("transport rank works", {
                                   observation.orientation =  "colwise", 
                                   method = method, is.X.sorted = TRUE)
   
+  testthat::skip_on_cran()
   # compare C order to R order
   testthat::expect_true(sum(x_idx-transx$tplan$from !=0) <= 2) #two obs flipped
 
@@ -179,7 +180,7 @@ testthat::test_that("transport rank works", {
   # testthat::expect_null(transx$cost)
   # testthat::expect_null(transy$cost)
   # testthat::expect_null(trans.nothing$cost)
-  testthat::skip_on_cran()
+  
   testthat::skip_on_ci()
   testthat::expect_equal(x_idx[507], transx$tplan$from[508])
   testthat::expect_equal(y_idx, transy$tplan$from)
